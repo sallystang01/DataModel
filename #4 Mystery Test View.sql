7cd4ac6b62e44ec96158563eebc372364644e57b
sallystang01 <hunterduerr@gmail.com>
2018-02-07 21:40:49 -0500

    Done for tonight

--- #4 Mystery Test View.sql
+++ #4 Mystery Test View.sql
@@ -1,11 +1,20 @@
 CREATE VIEW vwNotPaid
 AS
-select m.*, t.TransactionDate, pc.CardID from members m
+select m.MemberID, max(t.TransactionDate) [TransactionDate], pc.CardID, max(r.RenewalPrice) [RenewalPrice], max(r.RenewalType) [RenewalType], max(r.RenewalID) [RenewalID] from members m
 inner join
 paymentcard pc
 on pc.memberid = m.memberid
 inner join
 transactions t 
 on t.cardid = pc.cardid
-where t.transactiondate < GETDATE()
+inner join
+Renewal r
+on r.RenewalID = m.RenewalID
+where t.transactiondate < GETDATE() and NOT EXISTS (select TransactionDate
+from Transactions
+where TransactionDate = GETDATE()
+				)
+group by m.MemberID, pc.cardid
 GO
+
+	select * from vwNotPaid
--- #4 Mystery.sql
+++ #4 Mystery.sql
@@ -1,62 +1,98 @@
-
+GO
 ALTER PROCEDURE sp_Renewal
 
 	AS
 	BEGIN
 
-	DECLARE @MonthlyRenewalInitial date
-	DECLARE @QuarterlyRenewalInitial date
-	DECLARE @YearlyRenewalInitial date
+
 	DECLARE @MonthlyRenewal date
 	DECLARE @QuarterlyRenewal date
 	DECLARE @YearlyRenewal date
-
-	SET @MonthlyRenewalInitial = 
-	(select MemberID,max(dateadd(MONTH, 1 ,StartDate)) [Renewal Date]
-		from vwNotPaid
-		where RenewalID = 4
-		group by MemberID
-
+	DECLARE @CardID int
+	DECLARE @RenewalPrice smallmoney
+	DECLARE @SelectedMember int
+	DECLARE @TransactionDate date
+	
+	SET @TransactionDate =
+	(
+	select top 1 TransactionDate
+	from vwNotPaid
+	)
+	
+	SET @SelectedMember =
+	(
+	select top 1 MemberID
+	from vwNotPaid
 	)
 
-	SET @QuarterlyRenewalInitial = 
-	(select MemberID,max(dateadd(MONTH, 3 ,StartDate)) [Renewal Date]
-		from vwNotPaid
-		where RenewalID = 3
-		group by MemberID
-
+	SET @RenewalPrice = 
+	(
+	select RenewalPrice
+	from vwNotPaid
+	where @SelectedMember = MemberID
+	group by RenewalPrice
+	
 	)
 
-	SET @YearlyRenewalInitial = 
-	(select MemberID,max(dateadd(Year, 1 ,StartDate)) [Renewal Date]
-		from vwNotPaid
-		where RenewalID = 2
-		group by MemberID
-
+	SET @CardID = 
+	(
+	select CardID
+	from vwNotPaid
+	where @SelectedMember = MemberID
+	group by CardID
+	
 	)
+
+
 
 	SET @MonthlyRenewal = 
-	(select MemberID,max(dateadd(MONTH, 1 ,TransactionDate)) [Renewal Date]
+	(select  max(dateadd(MONTH, 1 ,TransactionDate)) [Renewal Date]
 		from vwNotPaid
-		where RenewalID = 4
+		where RenewalID = 4 and @SelectedMember = MemberID
 		group by MemberID
 
 	)
 
 	SET @QuarterlyRenewal = 
-	(select MemberID,max(dateadd(MONTH, 3 ,TransactionDate)) [Renewal Date]
+	(select  max(dateadd(MONTH, 3 ,TransactionDate)) [Renewal Date]
 		from vwNotPaid
-		where RenewalID = 3
+		where RenewalID = 3  and @SelectedMember = MemberID
 		group by MemberID
 
 	)
 
 		SET @YearlyRenewal = 
-	(select MemberID,max(dateadd(Year, 1 ,TransactionDate)) [Renewal Date]
+	(select max(dateadd(Year, 1 ,TransactionDate)) [Renewal Date]
 		from vwNotPaid
-		where RenewalID = 2
+		where RenewalID = 2 and @SelectedMember = MemberID
 		group by MemberID
 
 	)
 
-	select * from vwNotPaid
+	IF @MonthlyRenewal <= GETDATE()
+	BEGIN
+	
+	INSERT INTO Transactions (CardID, TransactionDate, Charge, Result)
+		VALUES (@CardID, @MonthlyRenewal, @RenewalPrice, 'Approved')
+END
+	IF @QuarterlyRenewal <= GETDATE()
+	BEGIN
+	
+	INSERT INTO Transactions
+		VALUES (@CardID, @QuarterlyRenewal, @RenewalPrice, 'Approved')
+END	
+	IF @YearlyRenewal <= GETDATE()
+	BEGIN
+
+	INSERT INTO Transactions
+		VALUES (@CardID, @YearlyRenewal, @RenewalPrice, 'Approved')
+
+
+END
+
+END
+GO
+
+	--select * from vwNotPaid
+
+	--select * from Transactions
